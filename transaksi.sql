742ef6faa2ffa0efa2e52b47fdd27c022e9517d7
M.Hafidz Ashshidiqi <125180024+HafidzAshshidiqi@users.noreply.github.com>
2023-09-22 12:09:52 +0700

    Delete transaksi.sql

--- transaksi.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Transaksi (
-    id_transaksi INT(20) PRIMARY KEY,
-    id_rekening INT(25),
-    tujuan_transaksi VARCHAR(20),
-    jumlah_transaksi INT(15),
-    tanggal_transaksi DATE,
-    jenis_transaksi VARCHAR(25),
-    FOREIGN KEY (id_rekening) REFERENCES Rekening_Bank(id_rekening)
-);
